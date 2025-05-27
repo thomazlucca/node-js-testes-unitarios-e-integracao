9f17b9e24938fe1863a84a75527b7ef2b9fcbb0f
Thomaz Lucca <thomazlucca@gmail.com>
2025-05-27 08:35:33 -0300

    relatorios, watch e coverage

--- test/folhaDePagamento.test.js
+++ test/folhaDePagamento.test.js
@@ -1,13 +1,15 @@
 import { somaHorasExtras, calculaDescontos } from "../index.js";
 
-test("Deve retornar a soma das horas extras", () => {
-  const esperado = 2500;
-  const retornado = somaHorasExtras(2000, 500);
-  expect(retornado).toBe(esperado);
-});
+describe("Testes dos cálculos de folha", () => {
+  it("Deve retornar a soma das horas extras", () => {
+    const esperado = 2500;
+    const retornado = somaHorasExtras(2000, 500);
+    expect(retornado).toBe(esperado);
+  });
 
-test("Deve descontar o valor do salário", () => {
-  const esperado = 2300;
-  const retornado = calculaDescontos(2500, 200);
-  expect(retornado).toBe(esperado);
+  it("Deve descontar o valor do salário", () => {
+    const esperado = 2300;
+    const retornado = calculaDescontos(2500, 200);
+    expect(retornado).toBe(esperado);
+  });
 });
